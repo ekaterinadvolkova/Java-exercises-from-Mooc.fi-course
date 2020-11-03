eb3bb131444511ed13f4467e4727cbdf2fc4c2dc
Kateridzhe <62830083+ekaterinadvolkova@users.noreply.github.com>
2020-11-03 15:41:15 +0300

    Add files via upload

---
+++ Week_6/part06-Part06_06.LongestInCollection/src/main/java/Main.java
@@ -0,0 +1,20 @@
+
+import java.util.ArrayList;
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        // Try out your class here
+        
+        SimpleCollection j = new SimpleCollection("characters");
+        System.out.println("Longest: " + j.longest());
+
+        j.add("magneto");
+        j.add("mystique");
+        j.add("phoenix");
+
+        System.out.println("Longest: " + j.longest());
+        
+    }
+}
---
+++ Week_6/part06-Part06_06.LongestInCollection/src/main/java/SimpleCollection.java
@@ -0,0 +1,44 @@
+
+import java.util.ArrayList;
+
+public class SimpleCollection {
+
+    private String name;
+    private ArrayList<String> elements;
+
+    public SimpleCollection(String name) {
+        this.name = name;
+        this.elements = new ArrayList<>();
+    }
+
+    public void add(String element) {
+        this.elements.add(element);
+    }
+
+    public ArrayList<String> getElements() {
+        return this.elements;
+    }
+    
+    public String longest(){
+        if (this.elements.isEmpty()) {
+            return null;
+        }
+        // create an object reference for the object to be returned
+        // its first value is the first object on the list
+        String returnObject = this.elements.get(0);
+
+    // go through the list
+        for (String prs: this.elements) {
+        // compare each object on the list
+        // to the returnObject -- we compare heights
+        // since we're searching for the tallest,
+
+            if (returnObject.length() < prs.length()) {
+                // if we find a taller person in the comparison,
+                // we assign it as the value of the returnObject
+                returnObject = prs;
+            }
+        }
+        return returnObject;
+    }
+}
